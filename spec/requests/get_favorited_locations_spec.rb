1a5f5650e4772189659f580ebed0936c1954cd05
abdullaqudrat <aqudrat630@gmail,com>
2018-11-07 09:00:16 -0700

    adds sad path test for no api key

--- spec/requests/get_favorited_locations_spec.rb
+++ spec/requests/get_favorited_locations_spec.rb
@@ -27,4 +27,20 @@ describe "get favorites api" do
     expect(favorite_response[0]['data']['attributes']['location']).to eq(favorite_1.location)
     expect(favorite_response[1]['data']['attributes']['location']).to eq(favorite_2.location)
   end
+  it "logged in user cannot get favorite locations by favorites api end point with NO API KEY" do
+    favorite_1 = Favorite.create(location: "Denver, CO, USA")
+    favorite_2 = Favorite.create(location: "Los Angeles, CA, USA")
+    user = User.create(email: "whatever@example.com", password: "password", password_confirmation: "password", favorites: [favorite_1, favorite_2])
+
+    post '/api/v1/sessions', headers: {"email": "#{user.email}", "password": "#{user.password}"}
+
+    expect(response).to be_successful
+
+    login_response = JSON.parse(response.body)
+    login_api_key = login_response['api_key']
+
+    get "/api/v1/favorites"
+
+    expect(response.status).to eq(401)
+  end
 end
