e5cba725663a95c4cfa48b1d7db36dea04e66803
abdullaqudrat <aqudrat630@gmail,com>
2018-11-07 13:32:53 -0700

    changes authorize api request/authenticate user

--- app/commands/authenticate_user.rb
+++ app/commands/authenticate_user.rb
@@ -12,8 +12,6 @@ class AuthenticateUser
 
   private
 
-  attr_accessor :email, :password
-
   def user
     user = User.find_by_email(@email)
     return user if user && user.authenticate(@password)
