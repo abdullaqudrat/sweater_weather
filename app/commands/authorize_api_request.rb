adde47b4eee7168afb61d53cac57df6030cfe833
abdullaqudrat <aqudrat630@gmail,com>
2018-11-07 13:33:32 -0700

    Revert "changes authorize api request/authenticate user"
    This reverts commit e5cba725663a95c4cfa48b1d7db36dea04e66803.

--- app/commands/authorize_api_request.rb
+++ app/commands/authorize_api_request.rb
@@ -10,21 +10,24 @@ class AuthorizeApiRequest
   end
 
   private
-    def user
-      @user ||= User.find(decoded_auth_token[:user_id]) if decoded_auth_token
-      @user || errors.add(:token, 'Invalid token') && nil
-    end
 
-    def decoded_auth_token
-      @decoded_auth_token ||= JsonWebToken.decode(http_auth_header)
-    end
+  attr_reader :headers
 
-    def http_auth_header
-      if @headers['api_key'].present?
-        return @headers['api_key'].split(' ').last
-      else
-        errors.add(:token, 'Missing token')
-      end
-      nil
+  def user
+    @user ||= User.find(decoded_auth_token[:user_id]) if decoded_auth_token
+    @user || errors.add(:token, 'Invalid token') && nil
+  end
+
+  def decoded_auth_token
+    @decoded_auth_token ||= JsonWebToken.decode(http_auth_header)
+  end
+
+  def http_auth_header
+    if headers['api_key'].present?
+      return headers['api_key'].split(' ').last
+    else
+      errors.add(:token, 'Missing token')
     end
+    nil
+  end
 end
